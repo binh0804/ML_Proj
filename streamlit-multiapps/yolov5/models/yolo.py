0aab1c715f7d5a5f754a83477265c330056204f2
knippink <20110252@student.hcmute.edu.vn>
2022-12-12 18:38:03 +0700

    lib

--- streamlit-multiapps/yolov5/models/yolo.py
+++ streamlit-multiapps/yolov5/models/yolo.py
@@ -21,7 +21,7 @@ if str(ROOT) not in sys.path:
 if platform.system() != 'Windows':
     ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from   common import *
+from   models.common import *
 from   experimental import *
 from   yolov5.utils.autoanchor import check_anchor_order
 from   yolov5.utils.general import LOGGER, check_version, check_yaml, make_divisible, print_args
