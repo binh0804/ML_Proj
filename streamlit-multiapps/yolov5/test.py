1b9eded813abf5caeb8f3549a74666e6305fdf85
knippink <20110252@student.hcmute.edu.vn>
2022-12-12 12:08:48 +0700

    yolo5

---
+++ streamlit-multiapps/yolov5/test.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+import platform
+import sys
+from pathlib import Path
+
+import torch
+import torch.backends.cudnn as cudnn
+import numpy as np
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, strip_optimizer, xyxy2xywh)
+# from utils.plots import Annotator, colors, save_one_box
+# from utils.torch_utils import select_device, time_sync
+
+# Import the necessary libraries
+from PIL import Image
+from numpy import asarray
+
+
+ 
+# load the image and convert into
+# numpy array
+
+
+#run2Die()
